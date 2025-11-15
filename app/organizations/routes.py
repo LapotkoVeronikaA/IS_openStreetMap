9fd092266de18e4f9778d834444e070d4ddfb9c4
Лапотько Вероника Александровна <70192100@online.muiv.ru>
2025-11-15 10:21:36 +0300

    добавлена страница для детального просмотра организации

--- app/organizations/routes.py
+++ app/organizations/routes.py
@@ -9,3 +9,9 @@ from . import organizations_bp
 def index():
     orgs = Organization.query.order_by(Organization.name).all()
     return render_template('organizations_index.html', organizations=orgs)
+
+@organizations_bp.route('/<int:org_id>')
+@permission_required_manual('view_organizations')
+def view_org(org_id):
+    org = Organization.query.get_or_404(org_id)
+    return render_template('organization_public_view.html', org=org)
