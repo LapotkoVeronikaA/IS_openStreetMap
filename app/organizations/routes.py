cd4a9e06b9151ad5c86739e316c4c0d5c24af60e
Лапотько Вероника Александровна <70192100@online.muiv.ru>
2025-11-16 15:26:00 +0300

    рефакторинг: реализовано сохранение расширенных данных организации

--- app/organizations/routes.py
+++ app/organizations/routes.py
@@ -27,9 +27,13 @@ def add_org():
 
         new_org = Organization(
             name=request.form.get('name'),
+            legal_name=request.form.get('legal_name'),
             org_type=request.form.get('org_type'),
             location=request.form.get('location'),
             head_of_organization=request.form.get('head_of_organization'),
+            website=request.form.get('website'),
+            main_phone=request.form.get('main_phone'),
+            main_email=request.form.get('main_email'),
             notes=request.form.get('notes'),
         )
         db.session.add(new_org)
@@ -52,9 +56,13 @@ def edit_org(org_id):
             return render_template('organization_form.html', org=org)
 
         org.name = request.form.get('name')
+        org.legal_name = request.form.get('legal_name')
         org.org_type = request.form.get('org_type')
         org.location = request.form.get('location')
         org.head_of_organization = request.form.get('head_of_organization')
+        org.website = request.form.get('website')
+        org.main_phone = request.form.get('main_phone')
+        org.main_email = request.form.get('main_email')
         org.notes = request.form.get('notes')
         
         db.session.commit()
