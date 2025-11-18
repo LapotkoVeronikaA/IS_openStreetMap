ce898c2f2bdbdde6ccda3ee5dc408ac567dfb5cf
Лапотько Вероника Александровна <70192100@online.muiv.ru>
2025-11-18 14:47:00 +0300

    реализована базовая логика отображения маркеров на карте

--- app/map/routes.py
+++ app/map/routes.py
@@ -2,5 +2,61 @@
 from flask import render_template, url_for, jsonify, flash, redirect
 from app.models import Organization
 from app.utils import check_user_permission, permission_required_manual
+from app.organizations.routes import get_files_for_org
 from . import map_bp
 import re
+
+@map_bp.route('/')
+@permission_required_manual('view_map')
+def show_map():
+    markers_data = []
+
+    organizations = Organization.query.filter(
+        Organization.latitude.isnot(None),
+        Organization.longitude.isnot(None)
+    ).order_by(Organization.name).all()
+
+    # Собираем уникальные типы для фильтров
+    org_types = sorted(list(set(o.org_type for o in organizations if o.org_type)))
+
+    for org in organizations:
+        # Упрощаем ключ для иконки
+        icon_key = re.sub(r'[^а-яА-Я0-9]', '', org.org_type) if org.org_type else "default"
+
+        markers_data.append({
+            "id": org.id,
+            "lat": org.latitude,
+            "lon": org.longitude,
+            "name": org.name,
+            "icon_key": icon_key,
+            "filter_data": {
+                "org_type": org.org_type,
+                "head": org.head_of_organization,
+            },
+            "details": {
+                'name': org.name,
+                'location': org.location,
+                'org_type': org.org_type or '-',
+                'legal_name': org.legal_name or '-',
+                'head_of_organization': org.head_of_organization or '-',
+                'website': org.website,
+                'main_phone': org.main_phone,
+                'main_email': org.main_email,
+                'total_employees': org.total_employee_count,
+                'departments': org.get_departments(),
+                'contacts': org.get_contacts(),
+                'edit_url': url_for('organizations.edit_org', org_id=org.id),
+                'view_url': url_for('organizations.view_org', org_id=org.id)
+            }
+        })
+    
+    user_perms = {
+        'can_manage_organizations': check_user_permission('manage_organizations')
+    }
+
+    return render_template(
+        'map.html', 
+        markers_data=markers_data,
+        user_perms=user_perms,
+        org_types=org_types
+    )
